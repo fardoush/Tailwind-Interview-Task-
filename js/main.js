f63769fee50a7af5968202381faa152d1605012a
Jahanara Fardoush <42909226+fardoush@users.noreply.github.com>
2022-07-22 00:46:47 +0600

    Spellon interview task
    Spellon interview task use to tailwind css

---
+++ js/main.js
@@ -0,0 +1,89 @@
+
+$(document).on('click','.content_1',function(e){
+
+  $('#communication').hide();
+  $('#community').hide();
+  $('#payment').hide();
+  $('#maintence').hide();
+  $('#report').hide();
+  $('#portal').hide();
+  $('#crm').show();
+  })
+  
+  
+  $(document).on('click','.content_3',function(e){
+  
+  $('#communication').hide();
+  $('#community').hide();
+  $('#payment').hide();
+  $('#maintence').hide();
+  $('#report').hide();
+  $('#crm').attr("style", "display: none !important");
+  $('#portal').show();
+  
+  })
+  
+  $(document).on('click','.content_7',function(e){
+  
+  $('#communication').show();
+  $('#community').hide();
+  $('#payment').hide();
+  $('#maintence').hide();
+  $('#report').hide();
+  $('#crm').attr("style", "display: none !important");
+  $('#portal').hide();
+  
+  })
+  
+  
+  $(document).on('click','.content_6',function(e){
+  
+  $('#communication').hide();
+  $('#community').show();
+  $('#payment').hide();
+  $('#maintence').hide();
+  $('#report').hide();
+  $('#crm').attr("style", "display: none !important");
+  $('#portal').hide();
+  
+  })
+  
+  $(document).on('click','.content_5',function(e){
+  
+  $('#communication').hide();
+  $('#community').hide();
+  $('#payment').show();
+  $('#maintence').hide();
+  $('#report').hide();
+  $('#crm').attr("style", "display: none !important");
+  $('#portal').hide();
+  
+  })
+  
+  $(document).on('click','.content_4',function(e){
+  
+  $('#communication').hide();
+  $('#community').hide();
+  $('#payment').hide();
+  $('#maintence').show();
+  $('#report').hide();
+  $('#crm').attr("style", "display: none !important");
+  $('#portal').hide();
+  
+  })
+  
+  $(document).on('click','.content_2',function(e){
+  
+  $('#communication').hide();
+  $('#community').hide();
+  $('#payment').hide();
+  $('#maintence').hide();
+  $('#report').show();
+  $('#crm').attr("style", "display: none !important");
+  $('#portal').hide();
+  
+  })
+  
+
+ 
+
